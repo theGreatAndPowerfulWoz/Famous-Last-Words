88125ec54be54956feb92fcd3810d9ff1d1aa8a0
Jacob.Wozniak <the.great.and.powerful.woz@gmail.com>
2017-11-29 16:21:59 +0100

    Updates
    Added text, added second page
    Figured out how to sort out the link

--- index.js
+++ index.js
@@ -4,9 +4,23 @@ $(document).ready(function() {
     });
 
     $('.cont').click(function(){
-        $('.one').slideUp(1500);
+        $('.one').fadeOut(1500, function(){
+            $('.playerTxt2').fadeTo(3000, 1, function(){
+                $('.cont2').fadeTo(2000, 1)
+            });
+        });        
     });
 
+    $('.cont2').click(function(){
+        $('.two').fadeOut(1500, function(){
+            $('.playerTxt3').fadeTo(3000, 1, function(){
+                $('.cont3').fadeTo(2000, 1)
+            });
+        });        
+    });
+    $('.cont3').click(function(){
+        $('three').fadeOut(1500, window.location.href = 'story2.html');
+    });
 
 });
     
